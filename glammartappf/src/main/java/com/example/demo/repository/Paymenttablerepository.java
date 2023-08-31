92e590e4a39f233b30b3b3782229794b2953f203
nidhiselvaraj <108916818+nidhiselvaraj@users.noreply.github.com>
2023-08-31 09:21:56 +0530

    Delete glammartappf directory

--- glammartappf/src/main/java/com/example/demo/repository/Paymenttablerepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Paymenttablemodel;
-
-public interface Paymenttablerepository extends JpaRepository<Paymenttablemodel, Integer>{
-
-}
--- glammartappf/src/main/java/com/example/demo/repository/Selectiontablerepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Selectiontablemodel;
-
-public interface Selectiontablerepository extends JpaRepository<Selectiontablemodel, Integer>{
-
-}
